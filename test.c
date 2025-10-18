e11c958cbf6d57f0e736430efade0ef0de19b514
Daniel Li <inflmts@gmail.com>
2025-10-18 14:25:02 -0400

    test: use 200ms delay instead of notify

--- test.c
+++ test.c
@@ -1,72 +1,95 @@
 #include <errno.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <time.h>
 #include <unistd.h>
 
 #define MAX_PEERS 32
 
 extern char **environ;
-static char env[64];
-static const char *filename = "PeerInfo.cfg";
+
+static void __attribute__((format(printf, 1, 2)))
+msg(const char *format, ...)
+{
+  va_list ap;
+  char buf[2048], *s;
+  va_start(ap, format);
+  vsnprintf(buf, sizeof(buf), format, ap);
+  va_end(ap);
+  for (s = buf; *s; ++s)
+    if (*s < 0x20 || *s == 0x7f)
+      *s = '?';
+  *(s++) = '\n';
+  write(2, buf, s - buf);
+}
+
+#define fatal_(format, ...) msg("fatal: " format ": %s", __VA_ARGS__)
+#define fatal(...) fatal_(__VA_ARGS__, strerror(errno)), _exit(-1)
 
 int main(int argc, char **argv)
 {
-  char arg[32];
-  char *args[3] = { "peer", arg, NULL };
-  int pipefd[2];
-  int id, ret, wstatus;
+  int id, ids[MAX_PEERS];
+  pid_t pid, pids[MAX_PEERS];
+
+  /* parse peerinfo file */
+  const char *const filename = "PeerInfo.cfg";
+  char line[1024];
   int num_peers = 0;
-  int ids[MAX_PEERS];
-  pid_t pids[MAX_PEERS];
-  pid_t pid;
-  if (pipe(pipefd)) {
-    fprintf(stderr, "fatal: pipe: %s\n", strerror(errno));
-    exit(-1);
-  }
-  sprintf(env, "P2P_NOTIFY_FD=%d", pipefd[1]);
-  putenv(env);
   FILE *f = fopen(filename, "r");
   if (!f) {
-    fprintf(stderr, "fatal: could not open '%s': %s\n", filename, strerror(errno));
-    exit(-1);
+    fatal("could not open '%s'", filename);
   }
-  char line[1024];
-  while (fgets(line, sizeof(line), f) && num_peers < MAX_PEERS) {
-    id = atoi(strtok(line, "\r\n "));
-    sprintf(arg, "%u", id);
+  while (fgets(line, sizeof(line), f)) {
+    if (num_peers == MAX_PEERS) {
+      fatal("only %d peers supported", MAX_PEERS);
+    }
+    char *token = strtok(line, "\r\n ");
+    if (token && (id = atoi(token))) {
+      ids[num_peers++] = id;
+    }
+  }
+  fclose(f);
+
+  /* launch peer processes */
+  char arg[32];
+  char *args[3] = { "peer", arg, NULL };
+  for (int i = 0; i < num_peers; ++i) {
+    msg("[starting peer %d]", ids[i]);
     pid = fork();
     if (pid < 0) {
-      fprintf(stderr, "fatal: fork: %s\n", strerror(errno));
+      fatal("fork");
       break;
     }
     if (pid == 0) {
-      /* child */
+      /* sleep for i x 200ms */
+      struct timespec ts = { i / 5, (i % 5) * 200000000 };
+      nanosleep(&ts, NULL);
+      sprintf(arg, "%d", ids[i]);
       execve("peer", args, environ);
-      fprintf(stderr, "fatal: failed to execute peer process: %s\n", strerror(errno));
-      _exit(-1);
+      fatal("failed to execute peer process %d", ids[i]);
     }
-    ids[num_peers] = id;
-    pids[num_peers] = pid;
-    read(pipefd[0], &arg, 1);
-    ++num_peers;
+    pids[i] = pid;
   }
-  fclose(f);
-  ret = 0;
+
+  /* wait for them all to exit */
+  int ret = 0;
+  int wstatus;
   while ((pid = wait(&wstatus)) != -1) {
     for (int i = 0; i < num_peers; ++i) {
       if (pids[i] == pid) {
         id = ids[i];
         if (WIFSIGNALED(wstatus)) {
-          fprintf(stderr, "[peer %u: %s]\n", id, strsignal(WTERMSIG(wstatus)));
-          ret = 1;
+          msg("[peer %d signaled: %s]", id, strsignal(WTERMSIG(wstatus)));
         } else {
-          fprintf(stderr, "[peer %u exited %d]\n", id, WEXITSTATUS(wstatus));
-          if (WEXITSTATUS(wstatus)) {
-            ret = 1;
-          }
+          msg("[peer %d exited %d]", id, WEXITSTATUS(wstatus));
         }
+        if (WIFSIGNALED(wstatus) || WEXITSTATUS(wstatus)) {
+          ret = 1;
+        }
+        break;
       }
     }
   }
